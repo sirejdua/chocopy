f6b7723bc7181f9d26c68393eaa6ec3b8285f235
Sirej Dua <sirejdua@gmail.com>
2019-01-15 21:22:12 -0800

    TODO: Regex

--- utils.py
+++ utils.py
@@ -3,116 +3,14 @@ MIN_INT:int = -4294967296
 
 # disclaimer
 # nothing here is tested 
-# yet.
-class Map:
-    #interface for map implementations
-    def put(self: "Map", key: "Object", val: "Object") -> object:
-        pass
 
-    def get(self: "Map", key: "Object") -> "Object":
-        return None
-
-class HashMap(Map):
-    # This is a hashmap
-    # the implementation is below
-    n:int = 8
-    size:int = 0
-    buckets:["LinkedList"] = []
-
-    def __init__(self: "HashMap") -> object:
-        i:int = 0
-        while (i < n):
-            self.buckets.append(LinkedList())
-            i = i + 1
-
-    def put(self: "HashMap", key: "Object", val: "Object") -> object:
-        h:int = 0
-        bucket_num:int = 0
-        if (size + 1 == n): #or a different condition 
-            self.realloc(n*2)
-            #double num buckets & reallocate items
-        h = key.__hash__()
-        bucket_num = (h % n + n) % n
-        self.buckets[bucket_num].push([key, val])
-        size += 1
-
-    def realloc(self: "HashMap", new_size:int) -> object:
-        # note
-        #   without a GC, this is kind of wasteful.
-        #   maybe the allocation should be in place
-        i:int = 0
-        new_buckets:["LinkedList"] = None
-        h:int = 0
-        bucket_num:int = 0
-        pair:["Object"] = None
-        chain:["LinkedList"] = None
-
-        while (i < new_size):
-            new_buckets.append(LinkedList())
-            i = i + 1
-        for chain in self.buckets:
-            for pair in chain:
-                h = pair[0].__hash__()
-                bucket_num = (h % new_size + new_size) % new_size
-                new_buckets[bucket_num].push(pair)
-        self.buckets = new_buckets
-        self.n = new_size
-
-    def get(self: "HashMap", key: "Object") -> "Object":
-        chain:"LinkedList" = None
-        h:int = 0
-        bucket_num:int = 0
-        h = key.__hash__()
-        bucket_num = (h % self.n + self.n) % self.n
-        chain = self.buckets[bucket_num]
-        while (not chain.end):
-            if h == chain.value[0].__hash__():
-                if key.__eq__(chain.value[0]):
-                    return chain.value
-            chain = chain.pointer
-        return None
-
-class LinkedList:
-    end:bool = True
-    pointer:"LinkedList" = None
-    value:"Object" = None
-
-    def __init__(self: "LinkedList") -> object:
-        pass
-
-    def push(self: "LinkedList", to_add: "Object") -> object:
-        new_pointer:"LinkedList" = None
-        new_pointer = pointer
-        self.pointer = LinkedList()
-        pointer.value = value
-        pointer.pointer = new_pointer
-        self.value = to_add
-        pointer.end = self.end
-        self.end = False
-
-    def get(self: "LinkedList") -> "Object":
-        return value
-
-    def get_index(self: "LinkedList", index: int) -> "Object":
-        if index < 0:
-            return None
-        elif index == 0:
-            return value
-        elif end:
-            return None
-        else:
-            return pointer.get_index(index-1)
-
-    def push_index(self: "LinkedList", to_add: "Object", index: int) -> object:
-        if index < 0:
-            return None
-        elif index == 0:
-            self.push(to_add)
-        elif end:
-            return None
-        else:
-            pointer.push_index(to_add, index-1)
-
+# Wrappers for primitive types
+# Wrappers inherit from Object, which is an object with __eq__ and __hash__
+# Other methods that Object will require may be determined based on the usage.
+# Here, we included __eq__ and __hash__ because these two methods are required
+# by the hash table contract.
+# Note: the only two requirements are the hashtable contract 
+# implement __eq__, and implement __hash__
 class Object:
     def __eq__(self:"Object", other:"Object") -> bool:
         return False
@@ -376,4 +274,127 @@ class Boolean(Object):
 
     def __eq__(self:"Boolean", other:"Boolean") -> bool:
         return self.value == other.value
+# # # # #
+#Data Structures#
+#
+#       ##    #   ###   #       ### ### ##  # # ### ###  # # ##  ###   ###
+#       # #  ###   #   ###      #    #  ##  # # #    #   # # ##  ##    #
+#       ##  #   #  #  #   #   ###    #  # # ### ###  #   ### # # ### ###
+#
+# # # # #
 
+class Map:
+    #interface for map implementations
+    def put(self: "Map", key: "Object", val: "Object") -> object:
+        pass
+
+    def get(self: "Map", key: "Object") -> "Object":
+        return None
+
+class HashMap(Map):
+    # This is a hashmap
+    # the implementation is below
+    n:int = 8
+    size:int = 0
+    buckets:["LinkedList"] = []
+
+    def __init__(self: "HashMap") -> object:
+        i:int = 0
+        while (i < n):
+            self.buckets.append(LinkedList())
+            i = i + 1
+
+    def put(self: "HashMap", key: "Object", val: "Object") -> object:
+        h:int = 0
+        bucket_num:int = 0
+        if (size + 1 == n): #or a different condition 
+            self.realloc(n*2)
+            #double num buckets & reallocate items
+        h = key.__hash__()
+        bucket_num = (h % n + n) % n
+        self.buckets[bucket_num].push([key, val])
+        size += 1
+
+    def realloc(self: "HashMap", new_size:int) -> object:
+        # note
+        #   without a GC, this is kind of wasteful.
+        #   maybe the allocation should be in place
+        i:int = 0
+        new_buckets:["LinkedList"] = None
+        h:int = 0
+        bucket_num:int = 0
+        pair:["Object"] = None
+        chain:["LinkedList"] = None
+
+        while (i < new_size):
+            new_buckets.append(LinkedList())
+            i = i + 1
+        for chain in self.buckets:
+            for pair in chain:
+                h = pair[0].__hash__()
+                bucket_num = (h % new_size + new_size) % new_size
+                new_buckets[bucket_num].push(pair)
+        self.buckets = new_buckets
+        self.n = new_size
+
+    def get(self: "HashMap", key: "Object") -> "Object":
+        chain:"LinkedList" = None
+        h:int = 0
+        bucket_num:int = 0
+        h = key.__hash__()
+        bucket_num = (h % self.n + self.n) % self.n
+        chain = self.buckets[bucket_num]
+        while (not chain.end):
+            if h == chain.value[0].__hash__():
+                if key.__eq__(chain.value[0]):
+                    return chain.value
+            chain = chain.pointer
+        return None
+
+class LinkedList:
+    end:bool = True
+    pointer:"LinkedList" = None
+    value:"Object" = None
+
+    def __init__(self: "LinkedList") -> object:
+        pass
+
+    def push(self: "LinkedList", to_add: "Object") -> object:
+        new_pointer:"LinkedList" = None
+        new_pointer = pointer
+        self.pointer = LinkedList()
+        pointer.value = value
+        pointer.pointer = new_pointer
+        self.value = to_add
+        pointer.end = self.end
+        self.end = False
+
+    def get(self: "LinkedList") -> "Object":
+        return value
+
+    def get_index(self: "LinkedList", index: int) -> "Object":
+        if index < 0:
+            return None
+        elif index == 0:
+            return value
+        elif end:
+            return None
+        else:
+            return pointer.get_index(index-1)
+
+    def push_index(self: "LinkedList", to_add: "Object", index: int) -> object:
+        if index < 0:
+            return None
+        elif index == 0:
+            self.push(to_add)
+        elif end:
+            return None
+        else:
+            pointer.push_index(to_add, index-1)
+
+
+### Utilities and Algorithms
+#
+#
+#
+# TODO: Regex parser and identifier
